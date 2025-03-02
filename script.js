ab962c29ab95e89e0f78b968da6928cc5a73c340
Dev <99797833+Ibrahim-Al-Najjar@users.noreply.github.com>
2025-03-02 08:35:06 +0300

    edit: when expand one collapse other

--- script.js
+++ script.js
@@ -5,6 +5,15 @@ document.addEventListener("DOMContentLoaded", function () {
     expandable.addEventListener("click", function (event) {
       const buttonImage = this.querySelector(".ExpandButton img");
 
+      // Collapse other expandables
+      expandables.forEach((otherExpandable) => {
+        if (otherExpandable !== this) {
+          otherExpandable.classList.remove("expandable-opened");
+          otherExpandable.querySelector(".ExpandButton img").src =
+            "assets/images/icon-plus.svg";
+        }
+      });
+
       this.classList.toggle("expandable-opened");
 
       if (this.classList.contains("expandable-opened")) {
